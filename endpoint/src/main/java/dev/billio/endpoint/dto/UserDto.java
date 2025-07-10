3d2b5cd596222535468bf8d73cfd7bd957326eb6
nullwa <walesebii@outlook.com>
2025-07-10 12:39:19 +0100

    user dto (to be used later on)

---
+++ endpoint/src/main/java/dev/billio/endpoint/dto/UserDto.java
@@ -0,0 +1,10 @@
+package dev.billio.endpoint.dto;
+
+// TODO : Add validation annotations to the DTO classes as needed
+public class UserDto {
+    public static class UserCreationDto {
+    }
+
+    public static class UserUpdateDto {
+    }
+}
